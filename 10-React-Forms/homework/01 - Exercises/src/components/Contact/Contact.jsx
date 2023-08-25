0d0b5c5b0cef23a12ea8579f962a1b1033c335b7
Ddeltoro97 <davison09.ddt@gmail.com>
2023-08-25 10:42:36 -0500

    forms

--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
+import { useState } from 'react';
 import './Contact.modules.css'
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
+export function validate(inputs){
+  let errors = {}
+
+  if(!inputs.name) {
+    errors.name = 'Se requiere un nombre';
+  }
+ 
+  if(!regexEmail.test(inputs.email)){
+    errors.email = 'Debe ser un correo electrónico';
+  }
+  
+  if(!inputs.message){
+    errors.message = 'Se requiere un mensaje';
+  }
+  
+  // console.log(errors)
+  return errors;
+}
+
 export default function Contact () {
-  return <div>Crear Formulario</div>
+
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (event) =>{
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value
+    })
+
+    setErrors(
+      validate({
+        ...inputs,
+        [event.target.name]: event.target.value
+      })
+    )
+  };
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+
+    const errores = [];
+
+    if(inputs.name.trim() == ''){
+      errores.push('name');
+    }
+    if(!regexEmail.test(inputs.email)){
+      errores.push('email');
+    }
+    if(inputs.message.trim() == ''){
+      errores.push('message');
+    }
+
+    if(errores.length === 0){
+      window.alert("Los datos fueron envíados")
+
+    }
+    else{
+      window.alert("Llena bien los formularios!")
+    }
+
+    // console.log(errores);
+  }
+
+  
+  return(
+ <form onSubmit={handleSubmit}>
+    <label htmlFor="text">Nombre: </label>
+    <input name = 'name' type='text' placeholder='Escribe tu nombre...' value={inputs.name} onChange={handleChange} className={errors.name && 'warning'}/>
+    {errors.name ? <p>{errors.name}</p> : ''}
+    <hr />
+
+    <label htmlFor="email">Email: </label>
+    <input name = 'email' type="text" placeholder='Escribe tu email...' value={inputs.email} onChange={handleChange} className={errors.name && 'warning'}/>
+    {errors.email ? <p>{errors.email}</p> : ''}
+    <hr />
+
+    <label htmlFor='text'>Mensaje: </label>
+    <input name = 'message' type="text" placeholder='Escribe tu mensaje...' value={inputs.message} onChange={handleChange} className={errors.name && 'warning'}/>
+    {errors.message ? <p>{errors.message}</p> : ''}
+    <hr />
+
+    <button type='Submit'>Enviar</button>
+ </form>
+  )
 }
